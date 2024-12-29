2f9403b16067cd4a6c2cdd218fb11ac5d9cf16a4
Birtalan Etele <etele.birtalan@gmail.com>
2024-12-29 18:14:13 +0200

    open a file using `with`

--- read_poem.py
+++ read_poem.py
@@ -1,6 +1,10 @@
-jabber = open('Jabberwocky.txt', 'r')
+# jabber = open('Jabberwocky.txt', 'r')
+#
+# for line in jabber:
+#     print(line.strip())
+#
+# jabber.close()
 
-for line in jabber:
-    print(line.strip())
-
-jabber.close()
+with open('Jabberwocky.txt', 'r') as jabber:
+    for line in jabber:
+        print(line.rstrip())
