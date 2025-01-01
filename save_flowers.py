581720e88f168ef0971db4b068b602294651da88
Birtalan Etele <etele.birtalan@gmail.com>
2025-01-01 19:26:39 +0200

    writing data to a text file

--- save_flowers.py
+++ save_flowers.py
@@ -20,15 +20,34 @@ data = [
     "Witch Hazel - Shrub",
 ]
 
-plants_filename = "flowers_print.txt"
+# plants_filename = "flowers_print.txt"
+#
+# with open(plants_filename, "w") as plants:
+#     for plant in data:
+#         print(plant, file=plants)
+#
+# new_list = []
+# with open(plants_filename) as plants:
+#     for plant in plants:
+#         new_list.append(plant.rstrip())
+#
+# print(new_list)
 
-with open(plants_filename, "w") as plants:
-    for plant in data:
-        print(plant, file=plants)
+# plants_filename = "flower_write.text"
+#
+# with open(plants_filename, "w") as plants:
+#     for plant in data:
+#         plants.write(plant)
+#
+# print(data)
+# string_representation = data.__str__()
+# print(type(string_representation))
 
-new_list = []
-with open(plants_filename) as plants:
-    for plant in plants:
-        new_list.append(plant.rstrip())
+filename = "test_numbers.txt"
+with open(filename, "w") as test:
+    for i in range(10):
+        print(i, file=test)
 
-print(new_list)
+with open(filename, "w") as test:
+    for i in range(10):
+        test.write(str(i) + "\n")
