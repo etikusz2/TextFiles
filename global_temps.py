c3fcd4e7d41d91099467d86c3d1af92b2e9c0008
Birtalan Etele <etele.birtalan@gmail.com>
2025-01-18 17:42:07 +0200

    parsing json data from the internet

--- global_temps.py
+++ global_temps.py
@@ -1,12 +1,18 @@
 import json
+import urllib.request
 
-json_data_source = 'data.json'
+json_data_source = 'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/globe/tavg/land_ocean/1/12/1850-2024'
 
-with open(json_data_source, encoding='utf-8') as data:
+# with open(json_data_source, encoding='utf-8') as data:
+with urllib.request.urlopen(json_data_source) as json_stream:
+    data = json_stream.read().decode('utf-8')
     anomalies = json.load(data)
 
 print(anomalies['description'])
 
 for year, value in anomalies['data'].items():
-    year, value = int(year), int(value)
+    year, value = int(year), float(value)
     print(f'{year} ...{value:6.2f}')
+print('*' * 80)
+
+# print(anomalies['citation'])
