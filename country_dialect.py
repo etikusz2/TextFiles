3c403fee985ff451246814b8555840cd1225fdef
Birtalan Etele <etele.birtalan@gmail.com>
2025-01-22 20:21:03 +0200

    csv dialect

--- country_dialect.py
+++ country_dialect.py
@@ -8,8 +8,21 @@ with open(input_filename, encoding='utf-8', newline='') as countries_data:
     for line in range(3):
         sample += countries_data.readline()
     country_dialect = csv.Sniffer().sniff(sample)
+    country_dialect.skipinitialspace = True
     countries_data.seek(0)
     country_reader = csv.reader(countries_data, dialect=country_dialect)
     for row in country_reader:
         print(row)
 
+print('*' * 80)
+attributes = ['delimiter',
+              'doublequote',
+              'escapechar',
+              'lineterminator',
+              'quotechar',
+              'quoting',
+              'skipinitialspace',
+              ]
+
+for attribute in attributes:
+    print(f'{attribute}: {repr(getattr(country_dialect, attribute))}')
